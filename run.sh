d72a04cdc08ea3a5e7b0b118a4e138f20f439f03
Gurjant Kalsi <gkalsi@google.com>
2023-06-01 23:57:55 +0000

    Convert ISO to QCOW on init
    Converts the ISO image to a QEMU QCOW2 image then resizes the
    capacity of the QCOW2 image to 30GiB so that apt update and
    apt upgrade succeed.

--- run.sh
+++ run.sh
@@ -21,6 +21,7 @@ QEMU_TAR_URL="https://download.qemu.org/qemu-8.0.0-rc4.tar.xz"
 QEMU_TAR="qemu-8.0.0-rc4.tar.xz"
 QEMU_DIRECTORY="./qemu-8.0.0-rc4"
 QEMU_BINARY="$QEMU_DIRECTORY/build/qemu-system-riscv64"
+QEMU_IMG="$QEMU_DIRECTORY/build/qemu-img"
 if ! [ -f $QEMU_BINARY ]; then
     # The binary doesn't exist, we need to build it.
     if ! [ -d $QEMU_DIRECTORY ]; then
@@ -57,6 +58,7 @@ fi
 # Download Ubuntu if it doesn't already exist.
 UBUNTU_REMOTE="https://cdimage.ubuntu.com/releases/22.04.2/release/ubuntu-22.04.2-preinstalled-server-riscv64+unmatched.img.xz"
 UBUNTU_IMG="ubuntu-22.04.2-riscv64.img"
+UBUNTU_QCOW=${UBUNTU_IMG/.img/.qcow2}
 UBUNTU_IMG_XZ="$UBUNTU_IMG.xz"
 if ! [ -f $UBUNTU_IMG ]; then
     if ! [ -f $UBUNTU_IMG_XZ ]; then
@@ -64,6 +66,18 @@ if ! [ -f $UBUNTU_IMG ]; then
     fi
     echo "Decompressing..."
     xz -dk $UBUNTU_IMG_XZ
+fi
+
+# The default preinstalled image reserves ~4.2GiB for the disk.
+# The actual disk is about ~2.xGiB used on boot which isn't really
+# large enough to do anything useful.
+# Even a single `apt update && apt upgrade` will fill up the disk
+# so we convert the disk image to a QCOW2 QEMU disk image and adjust
+# the capacity to some large number.
+if ! [ -f $UBUNTU_QCOW ]; then
+    $QEMU_IMG convert $UBUNTU_IMG $UBUNTU_QCOW
+    $QEMU_IMG resize $UBUNTU_QCOW 30G
+    $QEMU_IMG info $UBUNTU_QCOW
 fi
 
 
@@ -74,4 +88,4 @@ $QEMU_BINARY -machine virt -m 8G -smp cpus=8 -nographic        \
     -netdev user,id=net0                                       \
     -device virtio-net-device,netdev=net0                      \
     -device virtio-rng-pci                                     \
-    -drive file=$UBUNTU_IMG,format=raw,if=virtio
+    -drive file=$UBUNTU_QCOW,format=raw,if=virtio
